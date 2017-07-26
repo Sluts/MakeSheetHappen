266101419364503c9ef4d8b8f1ca4f88491d3f38
James <James>
2017-07-26 16:37:40 -0400

    stuff is different and whatnot

--- deepSheet.py
+++ deepSheet.py
@@ -6,14 +6,18 @@ upcs=[]
 prices=[]
 
 sometimesIbreakThings={}
-url="https://www.macphersonart.com/product/137187/Artists-Watercolor.html"
+#url="https://www.macphersonart.com/product/137187/Artists-Watercolor.html"
 #url='https://www.macphersonart.com/product/149250/Neon-Leather-Paint.html'
+url='https://www.macphersonart.com/product/132191/Artist-Bristle-Mottlers.html'
 
 response=ur.urlopen(url)
 data=response.read().decode("utf-8", errors="ignore")
 
 divs=re.findall(r'<div>(.*?)</div>', data)
 upc=re.findall(r'<div class="(.*?)</div>', data)
+desc=re.findall(r'<div class="prodDescription">(.*?)</div>', data)
+productName=re.findall(r'<div class="familyTitle">(.*?)</div>', data)
+manufact=re.findall(r'<a href=".*?">(.*?)</a>', data)
 
 for d in divs:
 	if "<br/>" in d:
@@ -31,14 +35,18 @@ for u in upc:
 		upcs.append((u[11:], 'upc'))
 		#print(u[11:])
 	elif 'RegPrice' in u:
-		#print(u[-4:])
-		#this is wrong - fix
-		prices.append(u[-4:])
+		#print(u[u.find('>')+2:])
+		prices.append(u[u.find('>')+2:])
 	else:
 		pass
+
+# for x in manufact:
+	# print(x)
+		
 for i in range(0,len(names)):
 	#upc -> ean/upc, name, sku, list price
 	sometimesIbreakThings[upcs[i][0]]=[upcs[i][1],names[i],skus[i], prices[i]]
 
-print(sometimesIbreakThings)
-	
+#print(sometimesIbreakThings)
+for k in sometimesIbreakThings.keys():
+	print(k+" -> "+str(sometimesIbreakThings[k])+"\n")
