09b15b1bb225eb7be59d73c88ad9b90438ab196e
Sluts <30280873+Sluts@users.noreply.github.com>
2017-08-16 19:50:57 -0400

    Delete rocheismybitch.py

--- rocheismybitch.py
+++
@@ -1,9 +0,0 @@
-import urllib.request as ur
-import re
-url='https://lamaisondupastel.com/shop/?cat=4'
-response=ur.urlopen(url)
-data=response.read().decode("utf-8", errors="ignore")
-siteRoot=re.findall(r'(.*?[c][o][m])[/]', url)
-divs=re.findall(r'<img src="(.*?)" />', data)
-for d in divs:
-	ur.urlretrieve(siteRoot[0]+d, "c:/users/james/pictures/henriroche/"+d[13:])
